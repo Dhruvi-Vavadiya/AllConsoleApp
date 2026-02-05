ed3085d3ef5031a25a66106fb3734a8f85e6ef8a
Dhruvi Vavadiya <dhruvivavadiya201@gmail.com>
2026-02-05 08:15:53 +0530

    first commit

---
+++ ConsoleApp1/StudnetCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    public class StudnetCollection <T>
+    {
+        string jjj = "Hello";
+        string ans = jjj.Reverse();
+        private List<T> myList;
+        private static int objectCount = 0;
+
+        public StudnetCollection() //ctor
+        {
+            myList = new List<T>();
+            objectCount++;
+        }
+
+       
+        public void AddList(T data)
+        {
+            this.myList.Add(data);
+        }
+        public void Show()
+        {
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+        public int Count
+        {
+            get { 
+                return myList.Count;
+            }
+        }
+    }
+}
---
+++ ConsoleAppEventDelegte/DdMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleAppEventDelegte.ddDeclare;
+
+namespace ConsoleAppEventDelegte
+{
+    internal class DdMethods
+    {
+        public static void ABCD(VoidDelegate del)
+        {
+            //Program.print(); //Problem
+            del();
+        }
+        //VoidDelegate
+        public static void print() { Console.WriteLine("program class print() method throgh get myclass abcd method using VoidDelegate name delegate"); }
+
+        //VoidDelWithParameter
+        private static void display(string msg) { Console.WriteLine("display method :-" + msg); }
+
+    }
+}
